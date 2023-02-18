e1851f5c74b9c8efa414664b798acd3cd5ff7a4b
Olena Reukova <kronverk01@gmail.com>
2023-02-18 14:21:57 +0000

    programmer-humour

---
+++ challenges/programmer-humour/index.js
@@ -0,0 +1,13 @@
+window.onload = renderFun;
+function renderFun() {
+  fetch("https://xkcd.now.sh/?comic=latest").then((response) =>
+    response
+      .json()
+      .then((data) => {
+        const img = document.querySelector("#container");
+        img.setAttribute("src", data.img);
+        console.log(data);
+      })
+      .catch((err) => console.error(`Error:`, err))
+  );
+}
